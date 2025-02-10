15c767c20d97f19ebab3a99c96b59e6b0bb6b4ca
jramma <jrmasip097@gmail.com>
2025-02-10 19:05:41 +0100

    first commit

---
+++ protected/models/Comment.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * This is the model class for table "{{comment}}".
+ *
+ * The followings are the available columns in table '{{comment}}':
+ * @property integer $id
+ * @property integer $post_id
+ * @property string $name
+ * @property string $email
+ * @property string $website
+ * @property string $content
+ * @property integer $status
+ * @property string $created_at
+ *
+ * The followings are the available model relations:
+ * @property Post $post
+ */
+class Comment extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '{{comment}}';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('post_id, name, email, content, status', 'required'),
+			array('post_id, status', 'numerical', 'integerOnly'=>true),
+			array('name, email, website', 'length', 'max'=>255),
+			array('created_at', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, post_id, name, email, website, content, status, created_at', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'post' => array(self::BELONGS_TO, 'Post', 'post_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'post_id' => 'Post',
+			'name' => 'Name',
+			'email' => 'Email',
+			'website' => 'Website',
+			'content' => 'Content',
+			'status' => 'Status',
+			'created_at' => 'Created At',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('post_id',$this->post_id);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('email',$this->email,true);
+		$criteria->compare('website',$this->website,true);
+		$criteria->compare('content',$this->content,true);
+		$criteria->compare('status',$this->status);
+		$criteria->compare('created_at',$this->created_at,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Comment the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
---
+++ protected/views/comment/_view.php
@@ -0,0 +1,43 @@
+<?php
+/* @var $this CommentController */
+/* @var $data Comment */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('post_id')); ?>:</b>
+	<?php echo CHtml::encode($data->post_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('website')); ?>:</b>
+	<?php echo CHtml::encode($data->website); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
+	<?php echo CHtml::encode($data->content); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('status')); ?>:</b>
+	<?php echo CHtml::encode($data->status); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
+	<?php echo CHtml::encode($data->created_at); ?>
+	<br />
+
+	*/ ?>
+
+</div>
